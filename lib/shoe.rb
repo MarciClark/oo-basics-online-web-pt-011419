a3657cb6045a7e6c8f1d4d93b5215f5805acac0c
Marci Clark <marcig612@gmail.com>
2019-02-01 23:56:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(properties)
+    @shoe = properties
+  end
+  
+  def properties
+    @properties
+  end
+  
+   def cobble(repaired)
+    puts "the show has been repaired"
+    if shoe_is_repaired
+  end
+  
+end
